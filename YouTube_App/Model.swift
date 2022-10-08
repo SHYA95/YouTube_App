ed2db923ce8b0937e17afd317810ceda55a8b530
SHYA95 <yassershrouk254@gmail.com>
2022-10-08 13:34:14 +0200

    Get the Youtube Api

---
+++ YouTube_App/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  YouTube_App
+//
+//  Created by Shrouk Yasser on 08/10/2022.
+//
+
+import Foundation
+class  Model{
+    func  GetVideos(){
+        //create a url object
+        let url = URL(string: Constants.API_URL)
+        guard url != nil else {
+            return 
+        }
+        //get a urlsession object
+        //get a date task from url session object
+        //kick off the task
+    }
+    
+}
